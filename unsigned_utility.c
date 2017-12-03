a647bcc5f7b7988b42062121e8cbf18f18d38306
Damia Hill <dhill@e1z2r7p45.42.us.org>
2017-12-02 23:33:39 -0800

    ahh the errant 1s

--- unsigned_utility.c
+++ unsigned_utility.c
@@ -6,7 +6,7 @@
 /*   By: dhill <dhill@student.42.us.org>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/30 22:50:44 by dhill             #+#    #+#             */
-/*   Updated: 2017/12/02 23:19:36 by dhill            ###   ########.fr       */
+/*   Updated: 2017/12/02 23:33:04 by dhill            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,18 +24,18 @@ int     ll_u_len(t_info *var, unsigned long long n)
         i = i / (unsigned long long)var->base;
         len++;
     }
-    if (n == 0 || i < (unsigned long long)var->base - 1)
+    if (n == 0 || i < (unsigned long long)var->base)
         len++;
     return (len);
 }
 
 int     u_prefix_len(t_info *var, unsigned long long num)
 {
-    if (var->base == 8)
-        return (1);
     if ((var->fl.pound_f == 0 || num == 0) && var->fl.p_prefix_f == 0)
         return (0);
-    if (var->base == 16)
+    if (var->base == 8)
+        return (1);
+	if (var->base == 16)
         return (2);
     return (0);
 }
